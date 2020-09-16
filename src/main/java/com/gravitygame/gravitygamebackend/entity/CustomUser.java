6cdf917ece35d8f8e949e5b442c5275eb3d1d3bf
Bálint Áts <atsbalint@gmail.com>
2020-09-16 11:40:05 +0200

    Create customUser entity

--- src/main/java/com/gravitygame/gravitygamebackend/entity/CustomUser.java
+++ src/main/java/com/gravitygame/gravitygamebackend/entity/CustomUser.java
@@ -1,4 +1,29 @@
 package com.gravitygame.gravitygamebackend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotEmpty
+    String username;
+
+    @NotEmpty
+    String password;
+
 }
