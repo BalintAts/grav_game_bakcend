651f369b2b346f2d9ea6e31232acc5655cc16cd4
Bálint Áts <atsbalint@gmail.com>
2020-09-18 10:38:59 +0200

    Minor refactor

--- src/main/java/com/gravitygame/gravitygamebackend/repository/UserRepository.java
+++ src/main/java/com/gravitygame/gravitygamebackend/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.gravitygame.gravitygamebackend.repository;
 
 import com.gravitygame.gravitygamebackend.entity.CustomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<CustomUser, Long> {
 
     Optional<CustomUser> findByUsername(String username);
--- src/main/java/com/gravitygame/gravitygamebackend/security/JwtTokenService.java
+++ src/main/java/com/gravitygame/gravitygamebackend/security/JwtTokenService.java
@@ -2,6 +2,7 @@ package com.gravitygame.gravitygamebackend.security;
 
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,6 +14,7 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+
 
 public class JwtTokenService {
 
